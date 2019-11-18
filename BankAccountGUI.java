791c9878df92a936b6f1627512b9e191aa3acf8d
Gillian McMahon <gmcmahon02@gmail.com>
2019-11-18 09:24:23 -0800

    Progress 11/18/2019

--- BankAccountGUI.java
+++ BankAccountGUI.java
@@ -11,17 +11,21 @@ import javafx.scene.control.Button;
 import javafx.scene.text.*;
 
 public class BankAccountGUI extends GridPane {
+    private TextField username;//User Name
+    private TextField pin;//PIN Number
+    private Text accountBox = new Text();//Account Box
+    private TextField deposit;//Deposit
+    private TextField withdrawl;//Withdraw
+    private TextField checkBalance;//Check Balance
+    private TextField newAcct;//Create Account
+    private TextField amount;//Amount
+    private BankAccount acct;
 
     public BankAccountGUI(){
-        //User Name
-        //PIN Number
-        //Account Box
-        //Deposit
-        //Withdraw
-        //Check Balance
-        //Create Account
-        //Amount
-        private BankAccount acct;
+        Font font = new Font(14);
+
+        Label inputUser = new Label("Username: ");
+
 
         public void deposit(){
             acct.Deposit();

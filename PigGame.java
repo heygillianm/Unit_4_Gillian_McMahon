96f737e9d7a0cfa4d64179df41518357f0ed901a
Michael George <georgem@wlwv.k12.or.us>
2019-12-10 09:53:28 -0800

    Unit 4 Assignment 12/10/2019

---
+++ PigGame.java
@@ -0,0 +1,11 @@
+public class PigGame {
+    PigPlayer player1;
+    PigPlayer player2;
+
+    public PigGame(){
+        player1 = new PigPlayer();
+        player2 = new PigPlayer();
+    }
+
+
+}
